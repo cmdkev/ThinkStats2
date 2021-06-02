252826e11f6b69861be8e8c01ea501dece7ef7a8
Kevin Bryson <kevinbryson15@gmail.com>
2021-06-01 21:08:43 -0400

    ch 1

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -12,13 +12,33 @@ import sys
 
 import nsfg
 import thinkstats2
+import gzip
 
+def readFemResp():
+    dctFile = '2002FemResp.dct'
+    datFile = '2002FemResp.dat.gz'
+    nrows = None
+
+    dct = thinkstats2.ReadStataDct(dctFile)
+    df = dct.ReadFixedWidth(datFile, compression='gzip', nrows=nrows)
+    return df
+
+def crossVal(resp):
+    dctFile = '2002FemPreg.dct'
+    datFile = '2002FemPreg.dat.gz'
+    nrows = None
+    dct = thinkstats2.ReadStataDct(dctFile)
+    preg = dct.ReadFixedWidth(datFile, compression='gzip', nrows=nrows)
 
 def main(script):
     """Tests the functions in this module.
 
     script: string script name
     """
+    resp = readFemResp()
+    
+    print(resp.pregnum.value_counts())
+
     print('%s: All tests passed.' % script)
 
 
